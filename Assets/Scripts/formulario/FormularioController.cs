f6555732be23f3ee4a94a5fbfc7b75d21f189547
Paula Pachon <pachon.paula1@gmail.com>
2024-11-28 22:53:24 -0500

    guardado de datos formulario y pantalla final

--- Assets/Scripts/formulario/FormularioController.cs
+++ Assets/Scripts/formulario/FormularioController.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro; // Importa TextMeshPro
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.SceneManagement; // Importar para cambiar de escena
+using System.Collections.Generic;
 
 public class FormularioController : MonoBehaviour
 {
@@ -12,6 +16,7 @@ public class FormularioController : MonoBehaviour
     public TMP_Dropdown dropdown6;
     public TMP_InputField comentarioInput; // Cambia a TMP_InputField para TextMeshPro
     public Button enviarButton;
+    public int Escena;  
 
     void Start()
     {
@@ -20,6 +25,7 @@ public class FormularioController : MonoBehaviour
 
     void OnEnviarButtonClick()
     {
+        // Obtener las selecciones de los dropdowns
         int seleccion1 = dropdown1.value + 1;
         int seleccion2 = dropdown2.value + 1;
         int seleccion3 = dropdown3.value + 1;
@@ -28,14 +34,58 @@ public class FormularioController : MonoBehaviour
         int seleccion6 = dropdown6.value + 1;
         string comentario = comentarioInput.text;
 
-        Debug.Log("Selecci?n 1: " + seleccion1);
-        Debug.Log("Selecci?n 2: " + seleccion2);
-        Debug.Log("Selecci?n 3: " + seleccion3);
-        Debug.Log("Selecci?n 4: " + seleccion4);
-        Debug.Log("Selecci?n 5: " + seleccion5);
-        Debug.Log("Selecci?n 6: " + seleccion6);
+        // Imprimir los datos en la consola para verificación
+        Debug.Log("Selección 1: " + seleccion1);
+        Debug.Log("Selección 2: " + seleccion2);
+        Debug.Log("Selección 3: " + seleccion3);
+        Debug.Log("Selección 4: " + seleccion4);
+        Debug.Log("Selección 5: " + seleccion5);
+        Debug.Log("Selección 6: " + seleccion6);
         Debug.Log("Comentario: " + comentario);
 
-        // Aqu? puedes agregar la l?gica para almacenar o enviar los datos
+        // Enviar los datos a PlayFab
+        EnviarDatosAPlayFab(seleccion1, seleccion2, seleccion3, seleccion4, seleccion5, seleccion6, comentario);
+
+        // Cambiar de escena después de enviar los datos
+        CambiarEscena();
+    }
+
+    void EnviarDatosAPlayFab(int seleccion1, int seleccion2, int seleccion3, int seleccion4, int seleccion5, int seleccion6, string comentario)
+    {
+        // Crear un diccionario con los datos a enviar
+        var data = new Dictionary<string, string>
+        {
+            { "Selección1", seleccion1.ToString() },
+            { "Selección2", seleccion2.ToString() },
+            { "Selección3", seleccion3.ToString() },
+            { "Selección4", seleccion4.ToString() },
+            { "Selección5", seleccion5.ToString() },
+            { "Selección6", seleccion6.ToString() },
+            { "Comentario", comentario }
+        };
+
+        // Llamada a PlayFab para actualizar los datos del usuario
+        PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest
+        {
+            Data = data
+        }, OnDatosEnviados, OnError);
+    }
+
+    // Callback cuando los datos se envían correctamente
+    void OnDatosEnviados(UpdateUserDataResult result)
+    {
+        Debug.Log("Datos enviados correctamente a PlayFab.");
+    }
+
+    // Callback en caso de error al enviar los datos
+    void OnError(PlayFabError error)
+    {
+        Debug.LogError("Error al enviar los datos a PlayFab: " + error.GenerateErrorReport());
+    }
+
+    void CambiarEscena()
+    {
+        // Cambiar a la escena especificada
+        SceneManager.LoadScene(Escena);
     }
 }
